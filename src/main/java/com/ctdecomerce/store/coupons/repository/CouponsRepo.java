83dc5a671fae37870b0a7f847afbccb433ebe223
Ismail3427 <ismailurfy@gmail.com>
2026-07-13 16:32:18 -0500

    remove the coupons

--- src/main/java/com/ctdecomerce/store/coupons/repository/CouponsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ctdecomerce.store.coupons.repository;
-
-import com.ctdecomerce.store.coupons.model.CouponsModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CouponsRepo extends JpaRepository<CouponsModel, UUID> {
-}
